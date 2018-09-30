d981bc32088f90204da9566138480e36b910a43f
Guntars Simanskis <guntars@diamondgraphics.ie>
2018-09-30 18:16:53 +0100

    Initial commit

---
+++ index.mjs
@@ -0,0 +1,9 @@
+import admin from 'firebase-admin';
+import serviceAccount from './_hidden/serviceAccountKey.json';
+
+const app = admin.initializeApp({
+    credential:  admin.credential.cert(serviceAccount),
+    databaseURL: 'https://test-project-780a1.firebaseio.com'
+});
+
+console.log(app);
